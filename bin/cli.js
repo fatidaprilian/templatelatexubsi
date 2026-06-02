1988efe50e3cdc89f2e7e3afa87d9e62168ac755
fatidaprilian <faridaprilian215@gmail.com>
2026-06-02 14:37:49 +0700

    feat: add LaTeX template files and CLI generator for project initialization

--- bin/cli.js
+++ bin/cli.js
@@ -59,6 +59,117 @@ function copyFolderSync(from, to) {
   });
 }
 
+const outlinesConfig = {
+  '1': {
+    type: 'magang',
+    title: 'Proyek Inovasi Perangkat Lunak',
+    folder: 'ti-proyek-inovasi',
+    babs: 6,
+    backmatter: [
+      '\\include{bagian-akhir-pkl/daftar-riwayat-hidup-pkl}',
+      '\\include{bagian-akhir-pkl/surat-keterangan-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-nilai-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-kuesioner}',
+      '\\include{bagian-akhir-pkl/lampiran-pkl}'
+    ]
+  },
+  '2': {
+    type: 'magang',
+    title: 'Analisa Program Berbasis Mobile',
+    folder: 'ti-mobile',
+    babs: 4,
+    backmatter: [
+      '\\include{bagian-akhir-pkl/daftar-riwayat-hidup-pkl}',
+      '\\include{bagian-akhir-pkl/surat-keterangan-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-nilai-pkl}',
+      '% \\include{bagian-akhir-pkl/lembar-kuesioner} % Tidak wajib untuk outline ini',
+      '\\include{bagian-akhir-pkl/lampiran-pkl}'
+    ]
+  },
+  '3': {
+    type: 'magang',
+    title: 'Jaringan Komputer',
+    folder: 'ti-jaringan',
+    babs: 4,
+    backmatter: [
+      '\\include{bagian-akhir-pkl/daftar-riwayat-hidup-pkl}',
+      '\\include{bagian-akhir-pkl/surat-keterangan-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-nilai-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-kuesioner}',
+      '\\include{bagian-akhir-pkl/lampiran-pkl}'
+    ]
+  },
+  '4': {
+    type: 'magang',
+    title: 'Analisis Sistem',
+    folder: 'ti-analisis-sistem',
+    babs: 4,
+    backmatter: [
+      '\\include{bagian-akhir-pkl/daftar-riwayat-hidup-pkl}',
+      '\\include{bagian-akhir-pkl/surat-keterangan-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-nilai-pkl}',
+      '\\include{bagian-akhir-pkl/lembar-kuesioner}',
+      '\\include{bagian-akhir-pkl/lampiran-pkl}'
+    ]
+  },
+  '5': {
+    type: 'skripsi',
+    title: 'Perancangan Program Science (Kode 156)',
+    metode: 'Individu',
+    folder: 'skripsi-ti-program-science',
+    babs: 5,
+    backmatter: [
+      '\\include{bagian-akhir/daftar-riwayat-hidup}',
+      '% \\include{bagian-akhir/lembar-bimbingan} % Tidak diperlukan untuk outline 156',
+      '\\include{bagian-akhir/surat-keterangan-riset}',
+      '\\include{bagian-akhir/bukti-plagiarisme}',
+      '\\include{bagian-akhir/lampiran}'
+    ]
+  },
+  '6': {
+    type: 'skripsi',
+    title: 'Penelitian Ilmiah (Kode 138)',
+    metode: 'Individu',
+    folder: 'skripsi-ti-penelitian-ilmiah',
+    babs: 5,
+    backmatter: [
+      '\\include{bagian-akhir/daftar-riwayat-hidup}',
+      '\\include{bagian-akhir/lembar-bimbingan}',
+      '\\include{bagian-akhir/surat-keterangan-riset} % Wajib',
+      '\\include{bagian-akhir/bukti-plagiarisme}',
+      '\\include{bagian-akhir/lampiran}'
+    ]
+  },
+  '7': {
+    type: 'skripsi',
+    title: 'Jaringan Komputer (Kode 137)',
+    metode: 'Individu',
+    folder: 'skripsi-ti-jaringan',
+    babs: 5,
+    backmatter: [
+      '\\include{bagian-akhir/daftar-riwayat-hidup}',
+      '\\include{bagian-akhir/lembar-bimbingan}',
+      '\\include{bagian-akhir/surat-keterangan-riset} % WAJIB ASLI',
+      '\\include{bagian-akhir/bukti-plagiarisme}',
+      '\\include{bagian-akhir/lampiran}'
+    ]
+  },
+  '8': {
+    type: 'skripsi',
+    title: 'Aplikasi StartUp (Kode 158)',
+    metode: 'Kelompok (maks. 2 orang)',
+    folder: 'skripsi-ti-startup',
+    babs: 4,
+    backmatter: [
+      '\\include{bagian-akhir/daftar-riwayat-hidup}',
+      '\\include{bagian-akhir/lembar-bimbingan}',
+      '\\include{bagian-akhir/surat-keterangan-riset}',
+      '\\include{bagian-akhir/bukti-plagiarisme}',
+      '\\include{bagian-akhir/lampiran}'
+    ]
+  }
+};
+
 function setupOutline(targetDir, choice) {
   const mainMagang = path.join(targetDir, 'main-magang.tex');
   const mainSkripsi = path.join(targetDir, 'main-skripsi.tex');
@@ -66,42 +177,71 @@ function setupOutline(targetDir, choice) {
   const outlinesDir = path.join(targetDir, 'outlines');
   
   const isMagang = parseInt(choice) <= 4;
-  let activeOutline = "";
+  
+  let config = outlinesConfig[choice];
+  if (!config) {
+    config = isMagang ? outlinesConfig['4'] : outlinesConfig['7'];
+  }
 
   if (isMagang) {
     fs.renameSync(mainMagang, mainFile);
     fs.unlinkSync(mainSkripsi);
-    if (choice === '1') activeOutline = "ti-proyek-inovasi";
-    else if (choice === '2') activeOutline = "ti-mobile";
-    else if (choice === '3') activeOutline = "ti-jaringan";
-    else if (choice === '4') activeOutline = "ti-analisis-sistem";
-    else activeOutline = "ti-analisis-sistem";
+    
+    // Cleanup Skripsi files
+    fs.rmSync(path.join(targetDir, 'bagian-akhir'), { recursive: true, force: true });
+    
+    const skripsiFiles = [
+      'cover-skripsi.tex', 'abstrak.tex', 'pengesahan.tex', 
+      'pernyataan-keaslian.tex', 'persetujuan-publikasi.tex', 
+      'pedoman-hak-cipta.tex', 'lembar-konsultasi.tex', 'persembahan.tex'
+    ];
+    skripsiFiles.forEach(f => {
+      const p = path.join(targetDir, 'bagian-awal', f);
+      if (fs.existsSync(p)) fs.unlinkSync(p);
+    });
+
   } else {
     fs.renameSync(mainSkripsi, mainFile);
     fs.unlinkSync(mainMagang);
-    if (choice === '5') activeOutline = "skripsi-ti-program-science";
-    else if (choice === '6') activeOutline = "skripsi-ti-penelitian-ilmiah";
-    else if (choice === '7') activeOutline = "skripsi-ti-jaringan";
-    else if (choice === '8') activeOutline = "skripsi-ti-startup";
-    else activeOutline = "skripsi-ti-jaringan";
+    
+    // Cleanup PKL files
+    fs.rmSync(path.join(targetDir, 'bagian-akhir-pkl'), { recursive: true, force: true });
+    
+    const pklFiles = ['cover-magang.tex', 'pengesahan-pkl.tex'];
+    pklFiles.forEach(f => {
+      const p = path.join(targetDir, 'bagian-awal', f);
+      if (fs.existsSync(p)) fs.unlinkSync(p);
+    });
   }
 
   let content = fs.readFileSync(mainFile, 'utf8');
   
-  // Activate the selected outline
-  const regex = new RegExp(`% \\\\include{outlines/${activeOutline}`, 'g');
-  content = content.replace(regex, `\\include{outlines/${activeOutline}`);
+  // Construct dynamic outline string
+  let outlineStr = `% =======================================================\n`;
+  if (config.type === 'skripsi') {
+    outlineStr += `% OUTLINE SKRIPSI: ${config.title}\n`;
+    outlineStr += `% Metode: ${config.metode}\n`;
+  } else {
+    outlineStr += `% OUTLINE PKL: ${config.title}\n`;
+  }
+  outlineStr += `% =======================================================\n`;
+  for (let i = 1; i <= config.babs; i++) {
+    outlineStr += `\\include{outlines/${config.folder}/bab${i}}\n`;
+  }
+  outlineStr += `% =======================================================`;
+
+  // Construct dynamic backmatter string
+  let backmatterStr = `%% =====================================================\n`;
+  backmatterStr += `%% BACKMATTER${config.type === 'magang' ? ' PKL' : ''}\n`;
+  backmatterStr += `%% =====================================================\n`;
+  backmatterStr += config.backmatter.join('\n');
   
-  // Deactivate defaults if they are not the selected one
-  if (isMagang && choice !== '4') {
-     content = content.replace(/\\include{outlines\/ti-analisis-sistem/g, `% \\include{outlines/ti-analisis-sistem`);
-  }
-  if (!isMagang && choice !== '7') {
-     content = content.replace(/\\include{outlines\/skripsi-ti-jaringan/g, `% \\include{outlines/skripsi-ti-jaringan`);
-  }
+  // Inject into main.tex
+  content = content.replace('% <<<OUTLINE_MARKER>>>', outlineStr);
+  content = content.replace('% <<<BACKMATTER_MARKER>>>', backmatterStr);
   
   fs.writeFileSync(mainFile, content);
-  keepOnly(outlinesDir, [activeOutline]);
+  keepOnly(outlinesDir, [config.folder]);
 }
 
 function keepOnly(dir, foldersToKeep) {
